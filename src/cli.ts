cabfddef85874287f376a6b28044370b2958562b
My Name <yrathi2@buffalo.edu>
2025-02-24 22:47:36 -0500

    adds github pull request template

--- src/cli.ts
+++ src/cli.ts
@@ -10,7 +10,7 @@ import { printResults, DeadCodeItem } from './utils.js';
 program
   .name('deadcode-detective')
   .description('Detect dead code in JavaScript/TypeScript and Python projects')
-  .version('1.0.0');
+  .version('1.0.2');
 
 program
   .command('detect')
